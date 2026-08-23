082347734cfc51f41cb413634edb35e48ea0b7ec
SinkSpace <vtamaichuk@yandex.ru>
2026-08-23 21:17:41 +0500

    2.1. test server

--- server.js
+++ server.js
@@ -8,10 +8,17 @@ const app = express();
 console.log('Библиотеки загружены');
 
 // инициализация сервера
-app.use = express.json;
+app.use(express.json());
 app.use(express.static('public'));
 
 console.log('Сервер инициализирован');
+
+// отправка веб-страниц
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    console.log('Главная страница отправлена');
+});
 
 // запуск сервера
 
